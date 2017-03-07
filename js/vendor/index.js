da275c7b967989a41e9ca83764a4fe18f1ce2a38
Diego Gonzalez <diego.gonzalez@enxendra.com>
2017-03-07 17:45:53 -0500

    Miscelanea

--- js/vendor/index.js
+++ js/vendor/index.js
@@ -2736,7 +2736,8 @@ var VexFlow = function () {
 
 			voice.tickables.forEach(function (tickable, i) {
 				pitches = [];
-
+                var notes = {"rules":[8,11,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53],"inclusive":true};
+                
 				if (tickable.noteType === 'n') {
 					notes[i].keys.forEach(function (key) {
 						// build array of pitches
